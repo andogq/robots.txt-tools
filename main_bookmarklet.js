91447283b2efd1a9b9f149b0a5ecb7e307600cac
Tom Anderson <portlester.tom@gmail.com>
2017-04-01 22:31:47 +1100

    Added bookmarklet version of script

---
+++ main_bookmarklet.js
@@ -0,0 +1,39 @@
+function robots() {
+	// Get base of url and site name
+	var url = location.origin;
+	var sitename = url.split(":")[1].substring(2);
+	
+	// XMLHttp request for url/robots.txt
+	var requestUrl = url + "/robots.txt";
+	var request = new XMLHttpRequest();
+	var file;
+	
+	request.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			file = this.responseText;
+			
+			// Parse file and generate HTML
+			file = file.split("\n");
+			var html = '<style>#file {font-family: monospace; background: white; position: absolute; top: 0; left: 0; bottom: 0; right: 0; padding: 20px;} p {color: black;} a {color: blue;}</style><div id="file">';
+			
+			// Loop through lines
+			for (i=0; i<file.length; i++) {
+				var line = file[i].split(" ");
+				if (line[0] == "Disallow:" || line[0] == "Allow:") {
+					html += "<p>" + line[0] + ' <a href="' + line[1] + '" target="_blank">' + line[1] + "</a></p>";
+				} else {
+					html += "<p>" + line.join(" ") + "</p>";
+				}
+			}
+			html += "</div>";
+			
+			// Load HTML into page
+			document.body.innerHTML = html;
+			document.title = sitename + "'s robots.txt";
+		}
+	}
+	request.open("GET", requestUrl, true);
+	request.send();
+}
+
+robots();
